e8636019033e2b117558dd89f7adf46b15fdee64
Mathias Valverde <mathias.valverde@omnilog.fr>
2023-09-15 10:39:59 +0200

    chore: architecture, redux, commitlint, etc

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Redux store
+import { store } from "@app/store";
+// Components
+import App from "@src/App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import * as path from "path";
 
-let faviconURL = '/favicon.svg'
-
-
+let faviconURL = "/favicon.svg";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,21 +12,43 @@ export default defineConfig({
     VitePWA({
       includeAssets: [faviconURL],
       manifest: {
-        theme_color: '#ffffff',
+        theme_color: "#ffffff",
         icons: [
           {
             src: faviconURL,
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
           },
           {
             src: faviconURL,
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-    })
-  ]
-})
+    }),
+  ],
+  resolve: {
+    alias: [
+      { find: "@admin", replacement: path.resolve(__dirname, "src/admin") },
+      { find: "@app", replacement: path.resolve(__dirname, "src/app") },
+      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
+      {
+        find: "@constants",
+        replacement: path.resolve(__dirname, "src/constants"),
+      },
+      { find: "@events", replacement: path.resolve(__dirname, "src/events") },
+      { find: "@home", replacement: path.resolve(__dirname, "src/home") },
+      { find: "@mocks", replacement: path.resolve(__dirname, "src/mocks") },
+      {
+        find: "@sharedComponents",
+        replacement: path.resolve(__dirname, "src/shared/components"),
+      },
+      { find: "@src", replacement: path.resolve(__dirname, "src") },
+      { find: "@styles", replacement: path.resolve(__dirname, "src/styles") },
+      { find: "@tests", replacement: path.resolve(__dirname, "tests") },
+      { find: "@utils", replacement: path.resolve(__dirname, "src/utils") },
+    ],
+  },
+});
