6890680e7dd450df006180696e19393f597fb0fb
Mathias Valverde <mathias.valverde@omnilog.fr>
2023-09-19 15:56:22 +0200

    fix: root/body

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { loadSentencesFromLocalStorage } from "@app/sentence/sentenceSlice";
 import type { RootState } from "@app/store";
 import SentenceForm from "@src/sentence/SentenceForm";
 import Navbar from "./sharedComponents/NavBar";
+import "@styles/index.scss";
 
 import '@styles/index.scss';
 
