83159cbd6897563e626dffa45255275baf1dc2f4
Mathias Valverde <mathias.valverde@omnilog.fr>
2023-09-26 15:10:32 +0200

    fix: scss button to bottom

--- src/sentences/components/SentenceFormPage.tsx
+++ src/sentences/components/SentenceFormPage.tsx
@@ -50,7 +50,7 @@ function SentenceForm() {
 
   return (
     <div className="SentenceFormPage">
-      <Header title="Personnes" />
+      <Header title="Personnes" goBack />
 
       <Formik
         initialValues={initialValues}
@@ -58,41 +58,48 @@ function SentenceForm() {
         onSubmit={handleSubmit}
       >
         {({ values, handleChange, isValid, dirty, isSubmitting }) => (
-          <Form>
-            <div className="field">
-              <label htmlFor="speaker">Personne</label>
-              <SelectField
-                id="speaker"
-                name="speaker"
-                value={values.speaker}
-                onChange={handleChange}
-              >
-                <option className="SelectField__placeholder" value="" disabled>
-                  Sélectionne une personne...
-                </option>
-                {people?.data
-                  ? people.data?.map((person: PeopleOptions) => (
-                      <option key={person.id} value={person.id}>
-                        {person.name}
-                      </option>
-                    ))
-                  : null}
-              </SelectField>
-              <ErrorMessage name="speaker" component="div" className="error" />
+          <Form className="SentenceFormPage__form">
+            <div>
+              <div className="SentenceFormPage__form-field">
+                <label htmlFor="speaker">Personne</label>
+                <SelectField
+                  id="speaker"
+                  name="speaker"
+                  value={values.speaker}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled>
+                    Sélectionne une personne...
+                  </option>
+                  {people?.data
+                    ? people.data?.map((person: PeopleOptions) => (
+                        <option key={person.id} value={person.id}>
+                          {person.name}
+                        </option>
+                      ))
+                    : null}
+                </SelectField>
+                <ErrorMessage name="speaker" component="div" className="error" />
+              </div>
+              <div className="SentenceFormPage__form-field">
+                <label htmlFor="review-text">Phrase</label>
+                <TextAreaField
+                  placeholder="Saisis la phrase..."
+                  id="sentence"
+                  name="sentence"
+                  value={values.sentence}
+                  onChange={handleChange}
+                  rows={5}
+                />
+                <ErrorMessage name="sentence" component="div" className="error" />
+              </div>
             </div>
-            <div className="field">
-              <label htmlFor="review-text">Phrase</label>
-              <TextAreaField
-                placeholder="Saisis la phrase..."
-                id="sentence"
-                name="sentence"
-                value={values.sentence}
-                onChange={handleChange}
-                rows={5}
-              />
-              <ErrorMessage name="sentence" component="div" className="error" />
-            </div>
-            <Button type="submit" disabled={isSubmitting || !isValid || !dirty}>
+
+            <Button
+              className="SentenceFormPage__form-button"
+              type="submit"
+              disabled={isSubmitting || !isValid || !dirty}
+            >
               Ajouter
               <IconAdd />
             </Button>
