2407fb8984e632ef3ffa58b4977952e71e3fad21
Mathias Valverde <mathias.valverde@omnilog.fr>
2023-09-26 15:46:45 +0200

    feat: select arrow down replaced

--- src/shared/components/Fields/SelectField/SelectField.tsx
+++ src/shared/components/Fields/SelectField/SelectField.tsx
@@ -1,11 +1,17 @@
 import clsx from 'clsx';
 
 import type { SelectFieldProps } from '@components/Fields/SelectField/SelectField.types';
+import IconSmallArrowDown from '@components/Icons/IconSmallArrowDown';
 
 import '@components/Fields/SelectField/SelectField.scss';
 
 function SelectField({ className, ...props }: SelectFieldProps): JSX.Element {
-  return <select className={clsx('SelectField', className)} data-testid="SelectField" {...props} />;
+  return (
+    <div className={clsx('SelectFieldWrapper', className)} data-testid="SelectField">
+      <select className="SelectFieldWrapper__field" {...props} />
+      <IconSmallArrowDown />
+    </div>
+  );
 }
 
 export default SelectField;
