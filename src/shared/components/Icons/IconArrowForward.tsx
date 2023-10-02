c7b337b1599c02282ca70149a4129636a7f223b6
Quentin MASBERNAT <quentin.masbernat@omnilog.fr>
2023-10-02 14:16:14 +0200

    style: sort icon properties

--- src/shared/components/Icons/IconArrowForward.tsx
+++ src/shared/components/Icons/IconArrowForward.tsx
@@ -2,11 +2,11 @@ function IconArrowForward(): JSX.Element {
   return (
     <svg
       aria-hidden
+      className="Icon Icon--arrow-forward"
       fill="currentColor"
       viewBox="0 0 30 30"
       xmlns="http://www.w3.org/2000/svg"
       xmlSpace="preserve"
-      className="Icon Icon--arrow-forward"
     >
       <path
         clipRule="evenodd"
